05b6bf30c916b8aa784373718ecdd2e60c806014
unknown <mangeshi@satvix.com>
2019-05-02 11:00:49 +0530

    COM-193 Update list of HIV ARV regimen

---
+++ sql/remove_drug.sql
@@ -0,0 +1,3 @@
+delete from drug where name in ('ABC/AZT+3TC+EFV', 'TDF/3TC+LPVr ou ATZ/r', 'TestAmoxilin 2545', 'AZT/3TC+NVP','ABC/AZT+3TC+ATV/r',
+'AZT+3TC+EFV', 'TDF+3TC/FTC+EFV', 'TDF+3TC+LPV/r ou ATV/r', 'TestAmoxicillin 000g', 'ABC+3TC+LPV/r', 'TDF/3TC/EFV', 'DRV/r+RAL+ETV', 'AZT+3TC+ATV/r',
+'TDF+3TC/FTC+NVP', 'TDF/FTC+EFV', 'AZT+3TC+LPVr+ ou AZT/r', 'TDF+3TC+ATV/r');
