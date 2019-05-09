85f84f6ba02a1e7f9ce7b556e155dbc82cb18a83
unknown <mangeshi@satvix.com>
2019-05-09 13:45:01 +0530

    COM-41 PECG Indicator 4: Total Number of PLWHA on ARV who took treatment  in the Month

--- sql/report_util_functions.sql
+++ sql/report_util_functions.sql
@@ -291,34 +291,20 @@ CREATE FUNCTION drugIsARV(
     DETERMINISTIC
 BEGIN
     return p_drugName IN (
-        "ABC/AZT+3TC+EFV",
-        "AZT/3TC+EFV",
-        "TDF/3TC+LPVr ou ATZ/r",
-        "ABC+3TC+NVP",
-        "ABC+3TC+EFV",
-        "AZT+3TC+LPV/r",
-        "AZT/3TC+NVP",
-        "ABC/AZT+3TC+ATV/r",
-        "AZT+3TC+LPV/r",
-        "AZT+3TC+EFV",
-        "ABC+3TC-LPV/r",
-        "TDF+3TC/FTC+EFV",
-        "TDF+3TC+LPV/r",
-        "AZT+3TC+LPV/r",
-        "ABC+3TC+LPV/r",
-        "TDF+3TC+LPV/r ou ATV/r",
-        "TDF/3TC+NVP",
-        "TDF+3TC+EFV",
-        "TDF/3TC/EFV",
-        "ABC+3TC+EFV",
-        "DRV/r+RAL+ETV",
-        "AZT+3TC+ATV/r",
-        "TDF+3TC/FTC+NVP",
-        "AZT+3TC+NVP",
-        "TDF/FTC+EFV",
-        "AZT+3TC+LPVr+ ou AZT/r",
-        "ABC+3TC+NVP",
-        "TDF+3TC+ATV/r"
+       "TDF+3TC+EFV",
+       "AZT/3TC+EFV",
+       "AZT+3TC+NVP",
+       "TDF/3TC+NVP",
+       "ABC+3TC+EFV",
+       "ABC+3TC+NVP",
+       "AZT+3TC+LPV/r",
+       "AZT+3TC+ATZ/r",
+       "ABC+3TC-LPV/r",
+       "ABC+3TC+ATZ/r",
+       "TDF+3TC+ATZ/r",
+       "TDF+3TC+LPVr",
+       "AZT+DRV+RTV",
+       "ABC+DRV+RTV"
     );
 END$$
 DELIMITER ;
@@ -439,4 +425,32 @@ BEGIN
         "ABC+3TC+NVP"
     );
 END$$
+DELIMITER ;
+
+-- patientPickedTotalNoOfARVDrugDuringReportingPeriod
+
+DROP FUNCTION IF EXISTS patientPickedTotalNoOfARVDrugDuringReportingPeriod;
+
+DELIMITER $$
+CREATE FUNCTION patientPickedTotalNoOfARVDrugDuringReportingPeriod(
+    p_patientId INT(11),
+    p_startDate DATE,
+    p_endDate DATE) RETURNS TINYINT(1)
+    DETERMINISTIC
+BEGIN
+
+    DECLARE result TINYINT(1) DEFAULT 0;
+
+    SELECT TRUE INTO result
+    FROM orders o
+    JOIN drug_order do ON do.order_id = o.order_id
+    JOIN concept c ON do.duration_units = c.concept_id AND c.retired = 0
+    JOIN drug d ON d.drug_id = do.drug_inventory_id AND d.retired = 0
+    WHERE o.patient_id = p_patientId AND o.voided = 0
+        AND drugIsARV(d.name)
+        AND o.date_activated BETWEEN p_startDate AND p_endDate
+    GROUP BY o.patient_id;
+
+    RETURN (result );
+END$$ 
 DELIMITER ;
