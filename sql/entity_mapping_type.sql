7dc6d0d4a033e8a886c3e8e1f47796f6f345bf57
Prashant <prashants@satvix.com>
2019-05-27 16:40:14 +0530

    COM-37 added hardcoded UUID's

--- sql/entity_mapping_type.sql
+++ sql/entity_mapping_type.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO entity_mapping_type
     (name,uuid,entity1_type,entity2_type)
 VALUES
-    ('visit_location',uuid(),'org.openmrs.VisitType','org.openmrs.Location');
+    ('visittype_location','bf9c4e41-4b69-49b4-986d-1aafdb7dcd55','org.openmrs.VisitType','org.openmrs.Location');
