6e041eb0bf1d378f2824b3882b324858aa55f4ea
lauravignoli <vignoli.laura@gmail.com>
2019-05-29 11:23:35 +0200

    COM-273 Update openElis scripts
    Add sample type other sample and delete all existing ranges before adding new.

--- postgresql/activate_departments_and_sample_types.sql
+++ postgresql/activate_departments_and_sample_types.sql
@@ -2,4 +2,4 @@
 update clinlims.test_section set is_active='N';
 update clinlims.test_section set is_active='Y' where description in ('Bacteriology Dept Orders','Haematology Dept Orders','Biochemistry Dept Orders','Parasitology Dept Orders');
 update clinlims.type_of_sample set is_active='N';
-update clinlims.type_of_sample set is_active='Y' where description in ('Arteriall blood','Blood culture bottle','CSF sample','ESR tube','Plasma EDTA','Plasma Sodium citrate','Puncture liquid sample','Sperm','Serum','Sputum','Stool','Urethral smear sample','Urine sample','Urine Sediments sample','Vaginal Cervical smear sample');
+update clinlims.type_of_sample set is_active='Y' where description in ('Arteriall blood','Blood culture bottle','CSF sample','ESR tube','Plasma EDTA','Plasma Sodium citrate','Puncture liquid sample','Sperm','Serum','Sputum','Stool','Urethral smear sample','Urine sample','Urine Sediments sample','Vaginal Cervical smear sample','Other Samples');
