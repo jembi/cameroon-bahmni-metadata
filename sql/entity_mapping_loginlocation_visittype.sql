edbdfc3b6c4cd1e911de66788060fabd87dcdcd3
Bailly RURANGIRWA <rubailly@gmail.com>
2019-07-18 11:03:56 +0200

    COM-461: Added mapping for login locations to visit types

---
+++ sql/entity_mapping_loginlocation_visittype.sql
@@ -0,0 +1,27 @@
+INSERT IGNORE INTO entity_mapping
+    (uuid,entity_mapping_type_id,entity1_uuid,entity2_uuid,date_created)
+VALUES
+    (uuid(),3,'0efa13f3-4351-4e9c-91fd-1d33a1833028','bc475481-050a-4938-b00a-e8dc6b9b4c01',now()),
+    (uuid(),3,'1144b765-2e6c-47b9-bb2d-e059ad1680a8','a71de1a2-aa43-496a-a533-13f47fad0129',now()),
+    (uuid(),3,'ff553570-8eca-4649-9c44-e1b06921796c','b1d913d1-70d8-4d6e-8aa6-e71f8b60a004',now()),
+    (uuid(),3,'0d9bf0c2-39f5-489a-a660-927f1ac97b7b','9b5f2493-fa75-44f9-80e4-bb389121ea0d',now()),
+    (uuid(),3,'c6e23237-5a0d-438a-9381-ac403d30054f','74c0393a-f5dd-4f4d-98bb-cedda584349b',now()),
+    (uuid(),3,'79157d5d-1e45-407d-bf15-ae8dc1674a70','f0a00d58-eeb8-4454-a41c-c8c2664313ec',now()),
+    (uuid(),3,'d1757c49-d3eb-4b36-bb2c-1d3faf238b20','660e38b5-2599-4ff5-b3fe-5c3fe0a5cd7e',now()),
+    (uuid(),3,'4f2e70b3-9b18-4773-994e-c67b31422722','b7d778bb-53ab-479c-a903-5bf18a62b0f3',now()),
+    (uuid(),3,'8568c575-16fc-4330-a3bb-c0460c15a298','9eb68046-b654-4884-b560-d1275caa84a7',now()),
+    (uuid(),3,'31b46228-0cb2-4e33-b8aa-4fcbae016adc','34cefcc2-0f50-4589-a487-9a68cdf3c11a',now()),
+    (uuid(),3,'ee0644c1-53e1-4c8c-bcd9-2308c608be60','dc0c872a-97d5-4731-aa00-ab8dd712e761',now()),
+    (uuid(),3,'ff6b443b-958b-4cf9-b187-d0f6da6b6d94','cb5f8bb5-df11-4c9d-974a-62a9e272ebf1',now()),
+    (uuid(),3,'0d414ccb-55a2-480f-93ff-c2af7061f0ba','19a762fa-143b-41fe-8ec0-d22630234863',now()),
+    (uuid(),3,'0607712c-aced-44da-8534-72d639bd9994','0cec2833-2914-4516-b6dc-2c8d58d924ec',now()),
+    (uuid(),3,'4863dd00-0543-40c1-9cb0-f019c11e86bf','8f82d1c4-43d1-4f2b-86fb-0484957927a0',now()),
+    (uuid(),3,'4dd3bdc2-38ab-450e-b36f-784ed6f8427d','1cb46ca4-cf55-4512-9058-eccd042d3a60',now()),
+    (uuid(),3,'df204697-a6c6-4768-a4e8-7082940fc8eb','d91e7c8a-0ebe-4792-a332-a175d881fe3a',now()),
+    (uuid(),3,'c7b372e3-ee2d-4e1e-b525-b89372399bbe','eda4bd55-7781-4a4d-b02a-40d7488ae586',now()),
+    (uuid(),3,'24a4ce15-b68c-4b97-bb6b-244af88c0505','6d8752aa-4233-4ec1-be79-6ca4bdc1e2a4',now()),
+    (uuid(),3,'4f1db0d2-8269-4e55-a81a-483674196b89','412d71de-64b5-46b5-a3d8-31e15ef1a075',now()),
+    (uuid(),3,'2f693094-b5f6-4e32-9c44-f079a5931c99','c458294a-d526-43f2-9bf3-447f8f963b5a',now()),
+    (uuid(),3,'4dbf4014-4f2c-4c74-ad3e-2f6df3f60c5d','976817df-1c8f-46da-8568-f61e5093f814',now()),
+    (uuid(),3,'c5854fd7-3f12-11e4-adec-0800271c1b75','bc475481-050a-4938-b00a-e8dc6b9b4c01',now()),
+    (uuid(),3,'60532776-4306-4258-bbad-7e9f9e37d350','531bed64-a661-4285-baaa-09a529a81cd0',now());
