f82362bf7eef231d40e9a561431148a29e998cbe
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2019-07-24 16:31:41 +0200

    COM-73 Remove queries retrieving the uuids
    The person uuids are already hard coded, thus there is no need to query and retrieve them. 
    This commit also updates queries that retrieve the user ids to filter by uuid instead username.

--- sql/create_users_and_user_roles.sql
+++ sql/create_users_and_user_roles.sql
@@ -1,42 +1,42 @@
 SET @nursesalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
-
 SET @doctorsalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
-
 SET @receptionsalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
 
-INSERT IGNORE INTO person (uuid,gender,date_created,creator)
-    VALUES ('3f64faba-ad31-11e9-a2a3-2a2ae2dbcce4','F',NOW(),'4');
+SET @nurse_person_uuid = '3f64faba-ad31-11e9-a2a3-2a2ae2dbcce4';
+SET @doctor_person_uuid = '3f64fd1c-ad31-11e9-a2a3-2a2ae2dbcce4';
+SET @reception_person_uuid = '3f64fe70-ad31-11e9-a2a3-2a2ae2dbcce4';
 
-SET @nurse_person_uuid = (SELECT person_id FROM person WHERE uuid = '3f64faba-ad31-11e9-a2a3-2a2ae2dbcce4');
+SET @nurse_user_uuid = '3bfbd6ec-ad1c-11e9-a2a3-2a2ae2dbcce4';
+SET @doctor_user_uuid = '3bfbdbba-ad1c-11e9-a2a3-2a2ae2dbcce4';
+SET @reception_user_uuid = '3bfbdd40-ad1c-11e9-a2a3-2a2ae2dbcce4';
+
+INSERT IGNORE INTO person (uuid,gender,date_created,creator)
+    VALUES (@nurse_person_uuid,'F',NOW(),'4');
 
 INSERT IGNORE INTO person_name (person_id,uuid,given_name,family_name) 
     VALUES (@nurse_person_uuid,'7459fb76-c81a-4a04-9578-656018d2828f','Nurse','');
 
 INSERT IGNORE INTO person (uuid,gender,date_created,creator)
-    VALUES ('3f64fd1c-ad31-11e9-a2a3-2a2ae2dbcce4','F',NOW(),'4');
-
-SET @doctor_person_uuid = (SELECT person_id FROM person WHERE uuid = '3f64fd1c-ad31-11e9-a2a3-2a2ae2dbcce4');
+    VALUES (@doctor_person_uuid,'F',NOW(),'4');
 
 INSERT IGNORE INTO person_name (person_id,uuid,given_name,family_name) 
     VALUES (@doctor_person_uuid,'c4d2632f-3ed2-4f5f-b37a-f92398cc5cc8','Doctor','');
 
 INSERT IGNORE INTO person (uuid,gender,date_created,creator)
-    VALUES ('3f64fe70-ad31-11e9-a2a3-2a2ae2dbcce4','F',NOW(),'4');
-
-SET @reception_person_uuid = (SELECT person_id FROM person WHERE uuid = '3f64fe70-ad31-11e9-a2a3-2a2ae2dbcce4');
+    VALUES (@reception_person_uuid,'F',NOW(),'4');
 
 INSERT IGNORE INTO person_name (person_id,uuid,given_name,family_name) 
     VALUES (@reception_person_uuid,'df9280ef-9f5a-4207-9668-9725355dfd01','Reception','');
 
 INSERT IGNORE INTO users (system_id,username,password,salt,date_created,uuid,person_id,creator)
     VALUES 
-        ('Nurse', 'Nurse', SHA2(CONCAT('Nurse#123',@nursesalt), 512), @nursesalt, NOW(), '3bfbd6ec-ad1c-11e9-a2a3-2a2ae2dbcce4',@nurse_person_uuid,'4'),
-        ('Doctor', 'Doctor', SHA2(CONCAT('Doctor#123',@doctorsalt), 512), @doctorsalt, NOW(), '3bfbdbba-ad1c-11e9-a2a3-2a2ae2dbcce4',@doctor_person_uuid,'4'),
-        ('Reception', 'Reception', SHA2(CONCAT('Reception#123',@receptionsalt), 512), @receptionsalt, NOW(), '3bfbdd40-ad1c-11e9-a2a3-2a2ae2dbcce4',@reception_person_uuid,'4');
+        ('Nurse', 'Nurse', SHA2(CONCAT('Nurse#123',@nursesalt), 512), @nursesalt, NOW(), @nurse_user_uuid, @nurse_person_uuid,'4'),
+        ('Doctor', 'Doctor', SHA2(CONCAT('Doctor#123',@doctorsalt), 512), @doctorsalt, NOW(), @doctor_user_uuid, @doctor_person_uuid, '4'),
+        ('Reception', 'Reception', SHA2(CONCAT('Reception#123',@receptionsalt), 512), @receptionsalt, NOW(), @reception_user_uuid,@reception_person_uuid,'4');
 
-SET @nurse_user_id = (SELECT user_id FROM users WHERE username = 'Nurse');
-SET @doctor_user_id = (SELECT user_id FROM users WHERE username = 'Doctor');
-SET @reception_user_id = (SELECT user_id FROM users WHERE username = 'Reception');
+SET @nurse_user_id = (SELECT user_id FROM users WHERE uuid = @nurse_user_uuid);
+SET @doctor_user_id = (SELECT user_id FROM users WHERE uuid = @doctor_user_uuid);
+SET @reception_user_id = (SELECT user_id FROM users WHERE uuid = @reception_user_uuid);
 
 INSERT IGNORE INTO provider (person_id,creator,date_created,uuid)
     VALUES
