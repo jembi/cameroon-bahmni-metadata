c954a39bd0e359fdbde80abae6686b01c34f2332
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2019-07-25 17:57:01 +0200

    COM-73 Insert users only if they do not exist already
    This commit adds logic to verify that a user does not exist before adding a new record. This is necessary because the column users.uuid does not have a unique constraint (therefore we cannot rely on the built in constraints to avoid duplicates)

--- sql/create_users_and_user_roles.sql
+++ sql/create_users_and_user_roles.sql
@@ -2,37 +2,59 @@ SET @nursesalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
 SET @doctorsalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
 SET @receptionsalt = (SHA2(SUBSTRING(MD5(RAND()), -10), 512));
 
-SET @nurse_person_uuid = '3f64faba-ad31-11e9-a2a3-2a2ae2dbcce4';
-SET @doctor_person_uuid = '3f64fd1c-ad31-11e9-a2a3-2a2ae2dbcce4';
-SET @reception_person_uuid = '3f64fe70-ad31-11e9-a2a3-2a2ae2dbcce4';
-
 SET @nurse_user_uuid = '3bfbd6ec-ad1c-11e9-a2a3-2a2ae2dbcce4';
 SET @doctor_user_uuid = '3bfbdbba-ad1c-11e9-a2a3-2a2ae2dbcce4';
 SET @reception_user_uuid = '3bfbdd40-ad1c-11e9-a2a3-2a2ae2dbcce4';
 
+SET @nurse_person_uuid = '564565ae-aec5-11e9-a2a3-2a2ae2dbcce4';
+SET @doctor_person_uuid = '56456824-aec5-11e9-a2a3-2a2ae2dbcce4';
+SET @reception_person_uuid = '56456978-aec5-11e9-a2a3-2a2ae2dbcce4';
+
 INSERT IGNORE INTO person (uuid, gender, date_created, creator)
     VALUES (@nurse_person_uuid, 'F', NOW(), '4');
 
+SET @nurse_person_id = (SELECT person_id FROM person WHERE uuid = @nurse_person_uuid);
+
 INSERT IGNORE INTO person_name (person_id, uuid, given_name, family_name) 
-    VALUES (@nurse_person_uuid, '7459fb76-c81a-4a04-9578-656018d2828f', 'Nurse', '');
+    VALUES (@nurse_person_id, '7459fb76-c81a-4a04-9578-656018d2828f', 'Nurse', '');
 
 INSERT IGNORE INTO person (uuid, gender, date_created, creator)
     VALUES (@doctor_person_uuid, 'F', NOW(), '4');
 
+SET @doctor_person_id = (SELECT person_id FROM person WHERE uuid = @doctor_person_uuid);
+
 INSERT IGNORE INTO person_name (person_id, uuid, given_name, family_name) 
-    VALUES (@doctor_person_uuid, 'c4d2632f-3ed2-4f5f-b37a-f92398cc5cc8', 'Doctor', '');
+    VALUES (@doctor_person_id, 'c4d2632f-3ed2-4f5f-b37a-f92398cc5cc8', 'Doctor', '');
 
 INSERT IGNORE INTO person (uuid, gender, date_created, creator)
     VALUES (@reception_person_uuid, 'F', NOW(), '4');
 
+SET @reception_person_id = (SELECT person_id FROM person WHERE uuid = @reception_person_uuid);
+
 INSERT IGNORE INTO person_name (person_id, uuid, given_name, family_name) 
-    VALUES (@reception_person_uuid, 'df9280ef-9f5a-4207-9668-9725355dfd01', 'Reception', '');
+    VALUES (@reception_person_id, 'df9280ef-9f5a-4207-9668-9725355dfd01', 'Reception', '');
+
+SET @nurse_exists = (SELECT 1 FROM users WHERE uuid = @nurse_user_uuid);
+SET @doctor_exists = (SELECT 1 FROM users WHERE uuid = @doctor_user_uuid);
+SET @reception_exists = (SELECT 1 FROM users WHERE uuid = @reception_user_uuid);
 
 INSERT IGNORE INTO users (system_id, username, password, salt, date_created, uuid, person_id, creator)
-    VALUES 
-        ('Nurse', 'Nurse', SHA2(CONCAT('Nurse#123', @nursesalt), 512), @nursesalt, NOW(), @nurse_user_uuid, @nurse_person_uuid, '4'),
-        ('Doctor', 'Doctor', SHA2(CONCAT('Doctor#123', @doctorsalt), 512), @doctorsalt, NOW(), @doctor_user_uuid, @doctor_person_uuid, '4'),
-        ('Reception', 'Reception', SHA2(CONCAT('Reception#123', @receptionsalt), 512), @receptionsalt, NOW(), @reception_user_uuid, @reception_person_uuid, '4');
+    SELECT 
+        'Nurse', 'Nurse', SHA2(CONCAT('Nurse#123', @nursesalt), 512), @nursesalt, NOW(), @nurse_user_uuid, @nurse_person_id, '4'
+    FROM DUAL
+    WHERE @nurse_exists IS NULL;
+
+INSERT IGNORE INTO users (system_id, username, password, salt, date_created, uuid, person_id, creator)
+    SELECT     
+        'Doctor', 'Doctor', SHA2(CONCAT('Doctor#123', @doctorsalt), 512), @doctorsalt, NOW(), @doctor_user_uuid, @doctor_person_id, '4'
+    FROM DUAL
+    WHERE @doctor_exists IS NULL;
+
+INSERT IGNORE INTO users (system_id, username, password, salt, date_created, uuid, person_id, creator)
+    SELECT 
+        'Reception', 'Reception', SHA2(CONCAT('Reception#123', @receptionsalt), 512), @receptionsalt, NOW(), @reception_user_uuid, @reception_person_id, '4'
+    FROM DUAL
+    WHERE @reception_exists IS NULL;
 
 SET @nurse_user_id = (SELECT user_id FROM users WHERE uuid = @nurse_user_uuid);
 SET @doctor_user_id = (SELECT user_id FROM users WHERE uuid = @doctor_user_uuid);
@@ -40,9 +62,9 @@ SET @reception_user_id = (SELECT user_id FROM users WHERE uuid = @reception_user
 
 INSERT IGNORE INTO provider (person_id, creator, date_created, uuid)
     VALUES
-        (@nurse_person_uuid, '4', NOW(), '1ad6aa26-adfa-11e9-a2a3-2a2ae2dbcce4'),
-        (@doctor_person_uuid, '4', NOW(), '1ad6ac88-adfa-11e9-a2a3-2a2ae2dbcce4'),
-        (@reception_person_uuid, '4', NOW(), '1ad6adf0-adfa-11e9-a2a3-2a2ae2dbcce4');
+        (@nurse_person_id, '4', NOW(), '1ad6aa26-adfa-11e9-a2a3-2a2ae2dbcce4'),
+        (@doctor_person_id, '4', NOW(), '1ad6ac88-adfa-11e9-a2a3-2a2ae2dbcce4'),
+        (@reception_person_id, '4', NOW(), '1ad6adf0-adfa-11e9-a2a3-2a2ae2dbcce4');
 
 INSERT IGNORE INTO user_role (user_id, role)
     VALUES 
