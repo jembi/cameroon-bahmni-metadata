f193ffb778d875018092f1ced98d735a1cc5489c
lauravignoli <vignoli.laura@gmail.com>
2019-08-08 11:50:09 +0200

    COM-505 Remove unnecessary file
    Deleted this file because it creates an entity_mapping_type that already exists in the system.

--- sql/entity_mapping_type_loginlocation_visittype.sql
+++
@@ -1,4 +0,0 @@
-INSERT IGNORE INTO entity_mapping_type
-    (name,uuid,entity1_type,entity2_type,date_created)
-VALUES
-    ('loginlocation_visittype','6af7aa94-12b2-11e6-8c00-080027d2adbd','org.openmrs.Location','org.openmrs.VisitType',now());
