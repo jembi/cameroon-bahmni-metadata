a413bd3649253bbaac78f8d86a7c66c4b1311bc0
lauravignoli <vignoli.laura@gmail.com>
2019-08-08 11:17:01 +0200

    COM-505 Remove non production script
    This script was added to re assign the correct concept id after a concept restore was done in a dev environment. It was removed because is not meant to be used in production.

--- post_installation_sql/update_program_attribute_type_comordities.sql
+++
@@ -1,8 +0,0 @@
-UPDATE program_attribute_type
-SET
-    datatype = 'org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype',
-    datatype_config = (
-        SELECT concept_id
-        FROM concept
-        WHERE uuid = '6cf6c41e-9f39-4d92-a78d-18252eba93ff')
-WHERE name = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES';
