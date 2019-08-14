c3aed98d7eaf7d79e22b9fb66635f3e33a1777e7
lauravignoli <vignoli.laura@gmail.com>
2019-08-14 09:54:26 +0200

    COM-511 Update comorbidities program attribute type
    Assign comorbidities concept set id to the program attribute type in sql script that will be ran after openmrs restarts.

--- sql/programlabels.sql
+++ sql/programlabels.sql
@@ -2,6 +2,7 @@ update program_attribute_type set description = 'PROGRAM_MANAGEMENT_ART_NUMBER',
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_STAGE', name = 'PROGRAM_MANAGEMENT_PATIENT_STAGE' where name = 'Stage';
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_TREATMENT_DATE', name = 'PROGRAM_MANAGEMENT_PATIENT_TREATMENT_DATE' where name = 'Date';
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES', name = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES' where name = 'Co-morbidites';
+update program_attribute_type set datatype = 'org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype', datatype_config = (select concept_id from concept where uuid = '6cf6c41e-9f39-4d92-a78d-18252eba93ff') where name = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES';
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_DOCTOR_IN_CHARGE', name = 'PROGRAM_MANAGEMENT_PATIENT_DOCTOR_IN_CHARGE' where name = 'Doctor';
 update program set name = 'HIV_PROGRAM_KEY' where name = 'HIV Program';
 update program set name = 'TB_PROGRAM_KEY' where name = 'TB Program';
