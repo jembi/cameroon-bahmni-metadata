1e225dfc924c2d7ca704cc3d152650fa2c40ae86
IipperMangesh <mangeshi@satvix.com>
2019-08-14 15:15:37 +0530

    COM-489 added new csv for 1st 2nd and 3rd line and updated sql with new concept id

--- sql/programlabels.sql
+++ sql/programlabels.sql
@@ -3,7 +3,7 @@ update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_STAG
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_TREATMENT_DATE', name = 'PROGRAM_MANAGEMENT_PATIENT_TREATMENT_DATE' where name = 'Date';
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES', name = 'PROGRAM_MANAGEMENT_PATIENT_COMORBIDITES' where name = 'Co-morbidites';
 update program_attribute_type set description = 'PROGRAM_MANAGEMENT_PATIENT_DOCTOR_IN_CHARGE', name = 'PROGRAM_MANAGEMENT_PATIENT_DOCTOR_IN_CHARGE' where name = 'Doctor';
-insert into program_attribute_type(name, description, datatype,creator, date_created, uuid) values("PROGRAM_MANAGEMENT_LABEL_THERAPEUTIC_LINE","PROGRAM_MANAGEMENT_LABEL_THERAPEUTIC_LINE","org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype", 1, NOW(), "a8bc4608-eaae-4610-a842-d83d6261ea49");
-update program_attribute_type set datatype_config=(select concept_id from concept where uuid="a8bc4608-eaae-4610-a842-d83d6261ea49") where uuid="a8bc4608-eaae-4610-a842-d83d6261ea49";
+insert ignore into program_attribute_type(name, description, datatype,creator, date_created, uuid) values("PROGRAM_MANAGEMENT_LABEL_THERAPEUTIC_LINE","PROGRAM_MANAGEMENT_LABEL_THERAPEUTIC_LINE","org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype", 1, NOW(), "397b7bc7-13ca-4e4e-abc3-bf854904dce3");
+update program_attribute_type set datatype_config=(select concept_id from concept where uuid="397b7bc7-13ca-4e4e-abc3-bf854904dce3") where name="PROGRAM_MANAGEMENT_LABEL_THERAPEUTIC_LINE";
 update program set name = 'HIV_PROGRAM_KEY' where name = 'HIV Program';
 update program set name = 'TB_PROGRAM_KEY' where name = 'TB Program';
