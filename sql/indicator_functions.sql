c56763bac7100d00b1bd5f9994441d294fa21ae0
Bailly Rurangirwa <rubailly@gmail.com>
2019-09-04 13:52:36 +0200

    Revert "Merge pull request #216 from jembi/COM-410"
    This reverts commit d8faf13c3a62feefea061f46af42ffe3c089ad07.

--- sql/indicator_functions.sql
+++ sql/indicator_functions.sql
@@ -624,38 +624,6 @@ WHERE
 END$$ 
 DELIMITER ;
 
-DROP FUNCTION IF EXISTS viralSuppression_Indicator1c;
-
-DELIMITER $$
-CREATE FUNCTION viralSuppression_Indicator1c(
-    p_startDate DATE,
-    p_endDate DATE,
-    p_startAge INT(11),
-    p_endAge INT (11),
-    p_includeEndAge TINYINT(1),
-    p_gender VARCHAR(1)) RETURNS INT(11)
-    DETERMINISTIC
-BEGIN
-    DECLARE result INT(11) DEFAULT 0;
-
-SELECT
-    COUNT(DISTINCT pat.patient_id) INTO result
-FROM
-    patient pat
-WHERE
-    patientGenderIs(pat.patient_id, p_gender) AND
-    patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
-    patientHasStartedARVTreatment12MonthsAgo(pat.patient_id, p_startDate, p_endDate) AND
-    patientAlreadyOnART(pat.patient_id, p_startDate) AND
-    patientIsVLSuppressedNotDocumented3MonthsBeforeReportingPeriod(pat.patient_id, p_startDate, p_endDate) AND
-    patientOnARVOrHasPickedUpADrugWithinExtendedPeriod(pat.patient_id, p_startDate, p_endDate, 0, 0) AND
-    patientIsNotDead(pat.patient_id) AND
-    patientIsNotLostToFollowUp(pat.patient_id) AND
-    patientIsNotTransferredOut(pat.patient_id);
-    RETURN (result);
-END$$ 
-DELIMITER ; 
-
 DROP FUNCTION IF EXISTS TREATMENT_Indicator3;
 
 DELIMITER $$
@@ -682,6 +650,7 @@ WHERE
     patientIsNotDead(pat.patient_id) AND
     patientIsNotLostToFollowUp(pat.patient_id) AND
     patientIsNotTransferredOut(pat.patient_id);
+
     RETURN (result);
 END$$ 
 DELIMITER ;
