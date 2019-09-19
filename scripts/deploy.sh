31d6e6b0b963e35741839ff0939c913cefc06897
lauravignoli <vignoli.laura@gmail.com>
2019-09-19 11:32:24 +0200

    Travis automated deployment update
    Updated the automated deployment to restart openmrs before running the sql and postgres scripts.

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ENV=$1
+MYSQL_PWD=$2
+ssh bahmni@cmr-$ENV.jembi.org "
+cd ~/git/cameroon-bahmni-metadata
+git reset --hard origin/$ENV
+git checkout $ENV
+git pull
+scripts/restart-openmrs.sh
+mysql -uroot -p$MYSQL_PWD openmrs < <(cat sql/*.sql);
+psql -U clinlims -d clinlims -a -f postgresql/activate_departments_and_sample_types.sql
+psql -U clinlims -d clinlims -a -f postgresql/add_test_ranges.sql
+/sbin/service bahmni-lab restart"
