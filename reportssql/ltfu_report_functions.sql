33efd5532bde8c469a6e14bfdd94237fc1782124
lauravignoli <vignoli.laura@gmail.com>
2020-01-14 11:33:19 +0200

    COM-588 Merge ltfu report files
    From now on, let's have the indicator and util functions together for each report.

--- reportssql/ltfu_report_functions.sql
+++ reportssql/ltfu_report_functions.sql
@@ -1,3 +1,38 @@
+-- LTFU Report
+
+DROP FUNCTION IF EXISTS LTFU_Indicator;
+
+DELIMITER $$
+CREATE FUNCTION LTFU_Indicator(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_gender VARCHAR(1)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+    COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+    patient pat
+WHERE
+    patientGenderIs(pat.patient_id, p_gender) AND
+    patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
+    patientHasEnrolledIntoHivProgram(pat.patient_id) = "Yes" AND
+    patientHasTherapeuticLine(pat.patient_id, 0) AND
+    patientHasStartedARVTreatmentBefore(pat.patient_id, p_endDate) AND
+    patientWasOnARVTreatmentByDate(pat.patient_id, p_startDate) AND
+    patientIsLostToFollowUp(pat.patient_id, p_startDate, p_endDate) AND
+    patientIsNotDead(pat.patient_id) AND
+    patientIsNotTransferredOut(pat.patient_id);
+
+    RETURN (result);
+END$$ 
+DELIMITER ;
+
 -- getDateMostRecentARVAppointmentThatIsBeforeReportEndDate
 
 DROP FUNCTION IF EXISTS getDateMostRecentARVAppointmentThatIsBeforeReportEndDate;
