9ceb4d5065e73cc191769860e4661a7be42f6f2b
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2020-01-24 17:06:30 +0200

    COM-639 Implement treatment indicator 4c Implements treatment indicator 4c

--- reportssql/treatment_report_functions.sql
+++ reportssql/treatment_report_functions.sql
@@ -232,6 +232,38 @@ WHERE
 END$$ 
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS TREATMENT_Indicator4c;
+
+DELIMITER $$
+CREATE FUNCTION TREATMENT_Indicator4c(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_gender VARCHAR(1)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+    COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+    patient pat
+WHERE
+    patientGenderIs(pat.patient_id, p_gender) AND
+    patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
+    patientHasEnrolledIntoHivProgram(pat.patient_id) = "Yes" AND
+    patientHasTherapeuticLine(pat.patient_id, 0) AND
+    patientHasStartedARVTreatmentBefore(pat.patient_id, p_endDate) AND
+    patientWasOnARVTreatmentByDate(pat.patient_id, p_startDate) AND
+    patientIsLostToFollowUp(pat.patient_id, p_startDate, p_endDate) AND
+    NOT patientHasStartedARVTreatmentAfter(pat.patient_id, TIMESTAMPADD(MONTH, -3, CURDATE()));
+
+    RETURN (result);
+END$$ 
+DELIMITER ;
+
 -- patientHasProgramOutcomeDeadWithinReportingPeriod
 
 DROP FUNCTION IF EXISTS patientHasProgramOutcomeDeadWithinReportingPeriod;
