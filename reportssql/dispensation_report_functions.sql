17930f9cc239055756d52935fbccbfa982acee3c
lauravignoli <vignoli.laura@gmail.com>
2020-02-11 13:57:50 +0200

    COM-93 Add AES & PA dispensation counts
    This was needed to consider all possible cases.

--- reportssql/dispensation_report_functions.sql
+++ reportssql/dispensation_report_functions.sql
@@ -75,8 +75,44 @@ BEGIN
 END$$ 
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS getNumberOfAESAndPAARVDispensed;
+
+DELIMITER $$
+CREATE FUNCTION getNumberOfAESAndPAARVDispensed(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_arvName VARCHAR(255)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+    SELECT count(DISTINCT o.order_id) INTO result
+    FROM orders o
+        JOIN drug_order do ON do.order_id = o.order_id
+        JOIN drug d ON d.drug_id = do.drug_inventory_id AND d.retired = 0
+        JOIN concept_name cn ON cn.concept_id = d.concept_id AND cn.locale = "en"
+    WHERE o.voided = 0
+        AND drugIsARV(d.concept_id)
+        AND cn.name = p_arvName
+        AND o.scheduled_date BETWEEN p_startDate AND p_endDate
+        AND drugOrderIsDispensed(o.patient_id, o.order_id)
+        AND patientAgeIsBetween(o.patient_id, p_startAge, p_endAge, p_includeEndAge)
+        AND patientReasonForConsultationIsUnplannedAid(o.patient_id)
+        AND IF (
+            patientIsAdult(o.patient_id),
+            drugIsAdultProphylaxis(d.concept_id),
+            drugIsChildProphylaxis(d.concept_id));
+
+    RETURN (result);
+END$$ 
+DELIMITER ;
+
 DROP FUNCTION IF EXISTS getNumberOfProphylaxisARVDispensed;
 
+-- exclude PA
 DELIMITER $$
 CREATE FUNCTION getNumberOfProphylaxisARVDispensed(
     p_startDate DATE,
@@ -102,14 +138,16 @@ BEGIN
         AND cn.name = p_arvName
         AND o.scheduled_date BETWEEN p_startDate AND p_endDate
         AND drugOrderIsDispensed(o.patient_id, o.order_id)
-        AND patientAgeIsBetween(o.patient_id, p_startAge, p_endAge, p_includeEndAge);
-
+        AND patientAgeIsBetween(o.patient_id, p_startAge, p_endAge, p_includeEndAge)
+        AND NOT patientReasonForConsultationIsUnplannedAid(o.patient_id);
+            
     RETURN (result);
 END$$ 
 DELIMITER ;
 
 DROP FUNCTION IF EXISTS getNumberOfPunctualAidARVDispensed;
 
+-- exclude AES
 DELIMITER $$
 CREATE FUNCTION getNumberOfPunctualAidARVDispensed(
     p_startDate DATE,
@@ -133,7 +171,11 @@ BEGIN
         AND cn.name = p_arvName
         AND o.scheduled_date BETWEEN p_startDate AND p_endDate
         AND drugOrderIsDispensed(o.patient_id, o.order_id)
-        AND patientAgeIsBetween(o.patient_id, p_startAge, p_endAge, p_includeEndAge);
+        AND patientAgeIsBetween(o.patient_id, p_startAge, p_endAge, p_includeEndAge)
+        AND IF (
+            patientIsAdult(o.patient_id),
+            NOT drugIsAdultProphylaxis(d.concept_id),
+            NOT drugIsChildProphylaxis(d.concept_id));
 
     RETURN (result);
 END$$ 
